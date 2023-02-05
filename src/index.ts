03bbad19c8044906e7ff5f4450065c6ef23d67f4
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-06 00:52:28 +0900

    feat: Apply Redis

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,56 @@ import Discord from "discord.js";
 import dotenv from "dotenv";
 import cron from "node-cron";
 
-import { channelList, updateChannelList } from "./channelList";
-import {
-  HELP_TEXT,
-  PREFIX,
-  COLOR,
-  COMMANDS,
-  RESPONSE_MESSAGE_MAP,
-} from "./constants";
-import getSaleInfo from "./crawling";
-import { channelIdList } from "./channelIdList";
-import { contains } from "./utils";
-import commandMap from "./commands";
+import { channelList, updateChannelList } from "./channelList.js";
+import { HELP_TEXT, PREFIX, COMMANDS } from "./constants/commands.js";
+import { RESPONSE_MESSAGE_MAP } from "./constants/message.js";
+import getSaleInfo from "./crawling.js";
+import { channelIdList } from "./channelIdList.js";
+import { contains } from "./utils.js";
+import commandMap from "./commands/index.js";
+import { redis } from "./redis/client.js";
+import { TSaleInfo } from "./types/index.js";
 
 dotenv.config();
 
-const client = new Discord.Client({
+const { NODE_ENV, TEST_BOT_TOKEN, BOT_TOKEN } = process.env;
+
+const EMBED_MESSAGE_COLOR = 0xff9726;
+const REDIS_KEY = "LIST";
+const DISCORD_TOKEN = NODE_ENV === "development" ? TEST_BOT_TOKEN : BOT_TOKEN;
+
+const discord = new Discord.Client({
   intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 
-client.on("ready", async () => {
-  console.log(`Logged in as ${client.user?.tag}`);
+discord.on("ready", async () => {
+  console.log(`Logged in as ${discord.user?.tag}`);
 
-  client.user?.setActivity("도움말: !help", {
+  discord.user?.setActivity(`도움말: ${PREFIX}help`, {
     type: Discord.ActivityType.Listening,
   });
   await updateChannelList();
   channelIdList.push(...channelList.map(({ channelId }) => channelId));
 
-  let pastInfo = await getSaleInfo();
   const sendNewInfo = async () => {
+    const pastInfo =
+      ((await redis.json.get(REDIS_KEY)) as TSaleInfo[]) ??
+      (await getSaleInfo());
     const currentInfo = await getSaleInfo();
-    currentInfo.reverse();
-    const newInfo = currentInfo.filter(
-      ({ url }) => !pastInfo.find(({ url: pastUrl }) => url === pastUrl)
-    );
+    const newInfo = currentInfo
+      .filter(
+        ({ url }) => !pastInfo.find(({ url: pastUrl }) => url === pastUrl)
+      )
+      .reverse();
+
+    if (!newInfo.length) return;
 
     newInfo.forEach((info) => {
       const targetChannels = channelList.filter(({ categories }) =>
         categories.some(({ category }) => category === info.category)
       );
       const embed = {
-        color: COLOR,
+        color: EMBED_MESSAGE_COLOR,
         title: info.title,
         url: info.url,
         author: { name: info.category },
@@ -51,22 +59,21 @@ client.on("ready", async () => {
         fields: [{ name: info.cost, value: info.shipping }],
       };
       targetChannels.forEach(({ channelId }) => {
-        client.channels.fetch(channelId).then((channel) => {
+        discord.channels.fetch(channelId).then((channel) => {
           if (!channel) return;
           if (channel.isTextBased())
             channel.send({ embeds: [embed] }).catch((e) => console.error(e));
         });
       });
     });
-
-    pastInfo = JSON.parse(JSON.stringify(currentInfo));
+    redis.json.set(REDIS_KEY, ".", currentInfo);
   };
-  cron.schedule("*/30 * * * * *", sendNewInfo);
+  cron.schedule("*/15 * * * * *", sendNewInfo);
 });
 
-client.on("messageCreate", async (message) => {
+discord.on("messageCreate", async (message) => {
   if (message.author.bot) return;
-  if (!message.content.startsWith(PREFIX)) return;
+  if (message.content.slice(0, PREFIX.length) !== PREFIX) return;
   const args = message.content.slice(PREFIX.length).split(" ");
   const command = args.shift()?.toLowerCase();
   if (!command) return;
@@ -78,4 +85,4 @@ client.on("messageCreate", async (message) => {
   await commandMap[command](message, args);
 });
 
-client.login(process.env.BOT_TOKEN);
+discord.login(DISCORD_TOKEN);
