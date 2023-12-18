92c3b6ceb2c8970be39cf63853e219cfc8f341d7
Kim Do Hyeong <kdh@m4nd4r1n.me>
2023-12-18 21:21:02 +0900

    feat(bot): send discord message

--- apps/bot/src/types.ts
+++ apps/bot/src/types.ts
@@ -6,3 +6,17 @@ export type HotDeal = {
   category: string;
   thumbnail: string;
 };
+export type Embed = {
+  title: string;
+  url: string;
+  color: number;
+  author: { name: string };
+  thumbnail: { url: string };
+  fields: { name: string; value: string }[];
+};
+
+export type WebhookBody = {
+  username: string;
+  avatar_url: string;
+  embeds: Embed[];
+};
