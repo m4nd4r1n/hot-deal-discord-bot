67658be4db8eae76251f9c4d285cab1c9f49239a
Kim Do Hyeong <kdh@m4nd4r1n.me>
2023-12-18 21:39:02 +0900

    feat(bot): apply cron scheduling

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,4 +1,19 @@
-import { prisma } from '@repo/database';
+import cron from 'node-cron';
 
-const categories = await prisma.category.findMany();
-console.log(categories);
+import { getNewPosts, savePosts } from '@/post';
+import { sendMessage } from '@/send';
+
+const run = async () => {
+  try {
+    const [newPosts, posts] = await getNewPosts();
+    if (!newPosts.length) return;
+
+    const postUrls = posts.map(({ url }) => ({ url }));
+
+    await Promise.all([sendMessage(newPosts), savePosts(postUrls)]);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+cron.schedule('* * * * *', run);
