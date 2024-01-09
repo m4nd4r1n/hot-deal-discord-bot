601f4069670c606de8531b7bfc83d0fc6b731a43
Kim Do Hyeong <kdh@m4nd4r1n.me>
2024-01-09 20:18:13 +0900

    style(web): page layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
 
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +18,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'bg-background mx-auto min-h-screen max-w-screen-md p-4 font-sans antialiased',
+          fontSans.className,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
