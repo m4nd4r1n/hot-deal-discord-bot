6b629807be89d8fc1607e24aab103e9c96a9879b
Kim Do Hyeong <kdh@m4nd4r1n.me>
2024-01-13 21:45:23 +0900

    feat(web): main page

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -12,3 +12,6 @@ export const CATEGORIES = Object.freeze([
 export const ITEMS = Object.freeze(
   CATEGORIES.map((category) => ({ id: category, label: category })),
 );
+
+export const DISCORD_WEBHOOK_GUIDE_URL =
+  'https://support.discord.com/hc/ko/articles/228383668-%EC%9B%B9%ED%9B%85%EC%9D%84-%EC%86%8C%EA%B0%9C%ED%95%A9%EB%8B%88%EB%8B%A4';
