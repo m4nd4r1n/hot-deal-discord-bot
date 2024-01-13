8d8a6bc1a4b7092fe341987ff478eba632f4a51c
Kim Do Hyeong <kdh@m4nd4r1n.me>
2024-01-13 15:16:11 +0900

    feat(web): add try catch wrapper

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const wrapTryCatch =
+  <T extends unknown[], U, W>(fn: (...args: T) => U, errorHandler: (error: Error) => W) =>
+  async (...args: T) => {
+    try {
+      return await fn(...args);
+    } catch (e) {
+      return errorHandler(e);
+    }
+  };
